480a796fde012130404a9ded43eccb88ce9f3edd
gtluszcz <gtluszcz@gmail.com>
2018-05-29 21:17:45 +0200

    check win for only active player

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -52,29 +52,16 @@
                 }
             },
             checkForWin(type){
-                let o_arr = []
-                let x_arr = []
+                let array = []
                 for (let arr of this.board){
                     for (let cell of arr){
-                        if (cell.value === 0)
-                            o_arr.push(cell)
-                        else if (cell.value === 1)
-                            x_arr.push(cell)
+                        if (cell.value === type)
+                            array.push(cell)
                     }
                 }
 
-                //console.log(o_arr)
-
-
-                if (type === 0){
-                    for(let cell of o_arr){
-                        this.checkNeighboursForWin(cell,0)
-                    }
-                }
-                else if (type === 1){
-                    for(let cell of x_arr){
-                        this.checkNeighboursForWin(cell,1)
-                    }
+                for(let cell of array){
+                    this.checkNeighboursForWin(cell,type)
                 }
             },
             checkNeighboursForWin(cell,type){
@@ -88,13 +75,11 @@
                         }
 
                         //check if exists
-                        if (cell.x+x<0 || cell.y+y<0)
+                        if (cell.x+x<0 || cell.y+y<0 || cell.x+x>=this.board.length || cell.y+y>=this.board[0].length)
                             continue
 
-                        //console.log(cell.x,cell.y)
-                        //if found neighbour
-                        //console.log(cell.x+x,cell.y+y,this.board[cell.x+x][cell.y+y].value)
 
+                        //if found neighbour
                         if (this.board[cell.x+x][cell.y+y].value === type){
                             console.log(x,y)
                             this.checkForWinInDirection(this.board[cell.x+x][cell.y+y],type,2,x,y)
